898ee15f2dce1fb1aa3e1d9f77cfa20dbf476ade
Beibarys Learner <beibarys.learner@gmail.com>
2024-05-01 17:11:31 +0500

    another practice

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 function App() {
     return (
         <>
-            <h1>Hedy Lamarr's Todos</h1>
-                <img
-                    src="https://i.imgur.com/yXOvdOSs.jpg"
-                    alt="Hedy Lamarr"
-                    className="photo"
-                />
-            <ul>
-                <li>Invent new traffic lights</li>
-                <li>Rehearse a movie scene</li>
-                <li>Improve the spectrum technology</li>
-            </ul>
+            <div className="intro">
+                <h1>Welcome to my website!</h1>
+            </div>
+            <p className="summary">
+                You can find my thoughts here.
+                <br />
+                <br />
+                <b>
+                    And <i>pictures</i>
+                </b>{" "}
+                of scientists!
+            </p>
         </>
-    )
+
+    );
 }
 
 export default App;
