4c5c4dd80a90c13954a624b0e05c2e7f50f876a2
Beibarys Learner <beibarys.learner@gmail.com>
2024-05-01 18:44:05 +0500

    handleClick

--- src/App.js
+++ src/App.js
@@ -1,43 +1,12 @@
-import { getImageUrl} from './util.js';
-
-function Avatar({ person, size}) {
-    // доступные пропсы отсюда
-    return (
-        <img
-            className="avatar"
-            src={getImageUrl(person)}
-            alt={person.name}
-            width={size}
-            height={size}
-        />
-    )
-}
-
 function App() {
+    function handleClick(){
+        alert("Salem React!");
+    }
+
     return (
-        <div>
-            <Avatar
-                size={100}
-                person={{
-                    name: 'Lin Lanying',
-                    imageId: '1bX5QH6'
-                }}
-            />
-            <Avatar
-                size={80}
-                person={{
-                    name: 'Lin Lanying',
-                    imageId: '1bX5QH6'
-                }}
-            />
-            <Avatar
-                size={50}
-                person={{
-                    name: 'Lin Lanying',
-                    imageId: '1bX5QH6'
-                }}
-            />
-        </div>
+        <button onClick={handleClick}>
+            Click Me!
+        </button>
     );
 }
 
--- src/readme.md
+++ src/readme.md
@@ -9,3 +9,12 @@
 1. передать пропсы в child component
 2. прочитать пропсы внутри child component
 
+### State
+
+Цель вывести сообщение 
+
+Сделаем это в 3 шага
+
+1. handleClick
+2. logic and add alert
+3. button 
