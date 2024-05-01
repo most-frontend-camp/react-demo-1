fb93a87f49370d990bae86beadc58af425bcfc9b
Beibarys Learner <beibarys.learner@gmail.com>
2024-05-01 17:09:18 +0500

    jsx practice

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-function Profile() {
-    return (
-        <img
-            src = "https://i.imgur.com/MK3eW3Am.jpg"
-            alt = "Kate Johnson"
-            />
-    )
-}
-
 function App() {
     return (
-        <section id="gallery">
-            <h1>Знаменитые ученые</h1>
-            <Profile />
-            <Profile />
-            <Profile />
-        </section>
-    );
+        <>
+            <h1>Hedy Lamarr's Todos</h1>
+                <img
+                    src="https://i.imgur.com/yXOvdOSs.jpg"
+                    alt="Hedy Lamarr"
+                    className="photo"
+                />
+            <ul>
+                <li>Invent new traffic lights</li>
+                <li>Rehearse a movie scene</li>
+                <li>Improve the spectrum technology</li>
+            </ul>
+        </>
+    )
 }
 
 export default App;
