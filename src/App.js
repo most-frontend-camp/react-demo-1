920b6b941f8b58d2652fe5e3e2a8209dbf93d922
Beibarys Learner <beibarys.learner@gmail.com>
2024-05-01 17:22:05 +0500

    fix the bug with image

--- src/App.js
+++ src/App.js
@@ -1,21 +1,49 @@
+const team = {
+    name: 'Real Madrid',
+    theme: {
+        backgroundColor:'black',
+        color: 'white'
+    }
+}
+
 function App() {
+
     return (
-        <>
-            <div className="intro">
-                <h1>Welcome to my website!</h1>
-            </div>
-            <p className="summary">
-                You can find my thoughts here.
-                <br />
-                <br />
-                <b>
-                    And <i>pictures</i>
-                </b>{" "}
-                of scientists!
-            </p>
-        </>
+        <div style={team.theme}>
+            <h1>{team.name}'s Todos</h1>
+            <img
+                className="img-fluid"
+                src="https://en.wikipedia.org/wiki/File:Real_Madrid_CF.svg"
+                alt="Real Madrid"
+            />
+            <ul>
+                <li>Toni Kroos</li>
+                <li>Luka Modric</li>
+                <li>Frederick Valverde</li>
+
+            </ul>
+        </div>
+
 
     );
+
+    /*    const name = 'Real Madrid';
+
+        return (
+          <h1>{name}'s team players </h1>
+        );*/
+
+    /*    const avatar = 'https://i.imgur.com/7vQD0fPs.jpg';
+        const description = 'Zara';
+
+        return (
+            <img
+                className="avatar"
+                src={avatar}
+                alt={description}
+            />
+        )*/
+
 }
 
 export default App;
